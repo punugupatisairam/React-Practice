a4da03f6ca32b2c2109a72bbc5936ab2a05414d1
sudhakar7711 <punugupatisairam@gmail.com>
2024-02-15 16:24:49 +0530

    planforbasicprogamming

---
+++ src/footer.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+function Footer(){
+    return <div>
+            {
+                <h1>This is Footer Section</h1>
+            }
+           </div>
+}
+export default Footer
---
+++ src/mainarea.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+function Mainarea(){
+    return <div>
+        <h1>This is Main Body Section</h1>
+    </div>
+}
+export default Mainarea 
