7dba15b51859afe40db88802a9108b64945477a7
sudhakar7711 <punugupatisairam@gmail.com>
2024-02-15 16:11:56 +0530

    todo

--- src/Todolist.jsx
+++ src/Todolist.jsx
@@ -1,1 +1,21 @@
-import React from "react";
+import React, { useRef } from "react";
+function Todo(){
+    var [todoitems,setTODOITEMS] = React.useState([])
+    var [x,setX] = React.useState('')
+    const addtodo = ()=>{
+        setTODOITEMS([...todoitems,x])
+    }
+    return <div>
+        <input type="text" name="" id="" onKeyUp={(e)=>{ setX(e.target.value)}} />
+        <button onClick={()=>{addtodo()}}>Add Item</button>
+
+        <ul>
+            {
+                todoitems.map((a)=>{
+                    return <li>{a}</li>
+                })
+            }
+        </ul>
+    </div>
+}
+export default Todo
