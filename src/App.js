fb6b18e5805ceeb19af1068b73578d690a48cc1d
sudhakar7711 <punugupatisairam@gmail.com>
2024-02-15 12:44:22 +0530

    1

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
+import Counter from './Counter';
 
 function App() {
   return (
-    <div className="App">
+    <div >
      <h1>Hai</h1>
-
+      <Counter/>
     </div>
   );
 }
