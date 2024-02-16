47f824cbbd67734b6abb801799bfe93d13ea3b11
sudhakar7711 <punugupatisairam@gmail.com>
2024-02-16 12:54:55 +0530

    ss

--- src/fakedata.jsx
+++ src/fakedata.jsx
@@ -4,22 +4,6 @@ import { useEffect } from "react";
 
 
 function Fake() {
-    var [countries, setCountries] = React.useState([]);
-    useEffect(() => {
-      axios.get("https://fakestoreapi.com/products").then((res) => {
-        setCountries(...countries,res.data);
-        console.log(countries);
-    });
-      
-    }, []);
-  
-    return <div className="App">
-      {
-        
-          countries.map((e)=>{
-            return <li>{e.name.common}</li>
-          })
-      }
-      </div>;
+    
   }
   export default Fake
