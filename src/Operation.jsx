0e16640dbf626784d10debdb035b9d7e32ec63fd
sudhakar7711 <punugupatisairam@gmail.com>
2024-02-23 18:47:49 +0530

    tododonebtn

--- src/Operation.jsx
+++ src/Operation.jsx
@@ -4,14 +4,14 @@ function Operation(props){
 return (
  <div className="todomaindiv" >
     <div className="newtodoitem1">
-    <span className="data"> {props.todo}</span>  &nbsp;&nbsp; 
-    <span className="status">Pending</span>
+    <span className="data"> {props.todo.title}</span>  &nbsp;&nbsp; 
+    <span className="status"> {props.todo.status}Pending</span>
 
     </div >
     <div className="newtodoitem2" >
     <button className="buttonde" onClick={()=>{props.delbtn(props.index)}}>Delete</button>
     {/* <button className="buttonu">Undo</button> */}
-    <button className="buttond" onClick={()=>{props.donebtn()}}>Done</button>
+    <button className="buttond" onClick={()=>{props.donebtn(props.todo)}}>Done</button>  
     <button className="buttone">Edit</button>
     </div>
     
--- src/Todolist.jsx
+++ src/Todolist.jsx
@@ -6,7 +6,12 @@ function Todo(){
     var [todoitems,setTODOITEMS] = React.useState([])
     var [x,setX] = React.useState('')
     const addtodo = ()=>{
-        setTODOITEMS([...todoitems,x]) 
+
+        var newobj ={
+            title:document.getElementById('inp').value,
+            status:false,
+        }
+        setTODOITEMS([...todoitems,newobj]) 
     }
     function delbtn(index){
     var temp = [...todoitems]
@@ -14,23 +19,35 @@ function Todo(){
     setTODOITEMS([...temp])
     }
 
-    function donebtn(){
+    function donebtn(ele){
         var temp = [...todoitems]
-        temp.style.display='line-through'
+        temp=temp.map((item)=>{
+            if(item.newobj.status===false){
+                item.newobj.status=true
+            }
+            else
+            {
+                item.newobj.status=false  
+            }
+            return item 
+
+        })
+        console.log()
+
         setTODOITEMS([...temp])
     }
     return <div className="todo">
         <Header ar={todoitems}/> 
         <h1 className="addtodotext" >📝 Add Your Todo's : 📝 </h1>
         <div className="inpdiv">
-        <input className="inp"  class="form-control " type="text" onKeyUp={(e)=>{ setX(e.target.value)}} placeholder="Enter Your Todo's" /> <br />
+        <input className="inp"  class="form-control " type="text" id="inp" placeholder="Enter Your Todo's" /> <br />
         <button  class="form-control bg-warning "  onClick={()=>{addtodo()}}>Add Item</button>
         <h1 className="newtodotext">All Your Todo's :</h1>
         </div>
         <ul>
             {
                 todoitems.map((todo,i)=>{
-                    return <Operation todo={todo} index={i} delbtn={delbtn} donebtn={donebtn}></Operation>
+                    return <Operation todo={todo} index={i} delbtn={delbtn} donebtn={donebtn} ></Operation>
                 })
             }
         </ul>
