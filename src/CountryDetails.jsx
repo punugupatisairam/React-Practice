3d4fa9eb40bf4f9ed3a416c2804d3ee841808f14
sudhakar7711 <punugupatisairam@gmail.com>
2024-02-23 20:26:09 +0530

    aa

--- src/CountryDetails.jsx
+++ src/CountryDetails.jsx
@@ -1,13 +1,30 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { useParams } from 'react-router-dom';
-
-
+import axios from 'axios';
 
 function CountryDetails() {
   let cparams = React.useParams();
   console.log(cparams);
+  let [countrydetails,setCountrydetails] = React.useState();
+
+  useEffect(()=>{
+    axios.get('https://restcountries.com/v3.1/name/{name}/${cparams.cname}?fullText=true').then((res)=>{
+          setCountrydetails(res.data)
+          console.log(res);
+    })
+
+  })
   return (
-    <div className='border border-black border-3 p-3'>CountryDetails</div>
+    <div className='border border-black border-3 p-3'>
+      {
+        countrydetails &&<div>
+                  <img src={countrydetails.flags.png} alt="" /> 
+                 <span>{countrydetails.name.common}</span>
+                 <span>{countrydetails.capital}</span>
+                 </div>
+        }
+      
+    </div>
   )
 }
 
