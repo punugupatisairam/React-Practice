15deff8584c92a7a686099808f1f0cb642e5a7e1
sudhakar7711 <punugupatisairam@gmail.com>
2024-02-26 17:34:48 +0530

    vv

--- src/App.js
+++ src/App.js
@@ -1,12 +1,56 @@
 import logo from './logo.svg';
 import './App.css';
+// import { Formik, useFormik } from 'formik';
+import { Formik } from 'formik';
 
-function App() {
-  return (
-    <div className="App">
-      
-    </div>
-  );
+
+function App() { 
+return (
+
+<Formik
+  initialValues = {{'fn':'','ln':'','age':''}}
+  onSubmit={(fv)=>{console.log(fv);}}
+  
+  >
+    {(userForm)=>{
+
+      return (
+        <form onSubmit={userForm.handleSubmit}>
+          <input type='text' placeholder='Enter First Name' name='fn' onChange={userForm.handleChange} /> <br/>
+          <input type='text' placeholder='Enter Last Name' name='ln' onChange={userForm.handleChange}/> <br/>
+          <input type='text' placeholder='Enter Age Name' name='age' onChange={userForm.handleChange}/> <br/> <br/>
+          <button type='sudmit'>save</button>
+        </form>
+      )   
+    }
+  } 
+</Formik>
+)
+
+
+
+
+
+
+  // var userForm = useFormik({
+  //   initialValues : {'fn':'','ln':'','age':''},
+  //   onSubmit:(fv)=>{console.log(fv);}
+  // })
+  // return (
+  //   <div >
+  //     {
+  //     <form onSubmit={userForm.handleSubmit} >
+  //         <label>First Name :</label>
+  //         <input name='fn' type='text' placeholder='Enter first name' onChange={userForm.handleChange}/> <br/>
+  //         <label>Last Name :</label>
+  //         <input name='ln' type='text' placeholder='Enter last name' onChange={userForm.handleChange}/> <br/>
+  //         <lable>Age :</lable >
+  //         <input  age='age' type='text' placeholder='Enter your age' onChange={userForm.handleChange}/> <br/>
+  //         <button type='submit'>Save</button>
+  //     </form>
+  //     }
+  //   </div>
+  // );
 }
 
 export default App;
