ccf555f774beb0ae7fed27c8cfcd9ee4725c4b1b
sudhakar7711 <punugupatisairam@gmail.com>
2024-03-02 18:00:01 +0530

    ss

--- src/LoginForm.jsx
+++ src/LoginForm.jsx
@@ -1,12 +1,27 @@
 import React from 'react'
 
+
 function LoginForm() {
     let formik = ({
-        
+        intialvalues:{
+          firstname:'',
+          password:''
+        },
+        onSubmit:((values)=>{
+            console.log(values);
+        })
     })
     
   return (
-    <div>LoginForm</div>
+    <div>
+      <form onSubmit={formik.onSubmit} >
+          <label htmlFor="">Name :</label>
+          <input type="text" onChange={formik. ()} />
+          <label htmlFor="">Password :</label>
+          <input type="text"  onChange={formik.onChange()} />
+          <button type='submit'>Login</button>
+      </form>
+    </div>
   )
 }
 
