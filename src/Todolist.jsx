9c579fed83bbbdca5d2af80e391d8354fd52283d
sudhakar7711 <punugupatisairam@gmail.com>
2024-03-09 16:47:07 +0530

    nott

--- src/Todolist.jsx
+++ src/Todolist.jsx
@@ -4,19 +4,27 @@ import { connect } from "react-redux";
 function Todolist(props) {
   console.log(props);
   var [newtodo, setNewtodo] = React.useState();
+  var [filterTodo,setFilterTodo] = React.useState(null)
   console.log(props);
+  function updatefilter(e){
+    // setFilterTodo(e.target.value)
+    props.dispatch({type:e.target.value});
+  }
   return (
     <div className="mystyle"> 
       <input type="text" onChange={(event) => {return setNewtodo(event.target.value);}}/>
-      <button onClick={() => { props.dispatch({ type: "ADDTODO", payload: newtodo });}}>Add Todo</button>
+      <button onClick={() => { props.dispatch({ type: "ADDTODO", payload: newtodo });}}>Add Todo</button><br />
+      <input type="radio" name='filter' value='All' onChange={(event)=>updatefilter(event)} />:All Todos
+      <input type="radio" name='filter' value='Done'  onChange={(event)=>updatefilter(event)} />:Completed
+      <input type="radio" name='filter' value='NotDone'  onChange={(event)=>updatefilter(event)} />:Pending
 
 
-      { props.todolist.todos.map((todo) => { 
+      { props.todolist.filteredTodos?.map((todo,i) => { 
         return ( 
           <div> 
             <span> 
               <span className={todo.status && 'strike'}>{todo.title}</span>    
-              <button onClick={()=>{props.dispatch({type:"DELETE",payload:todo.title})}}>Delete</button> 
+              <button onClick={()=>{props.dispatch({type:"DELETE",payload:i})}}>Delete</button> 
               <button onClick={()=>{props.dispatch({type:"DONE",payload:todo.title})}}>Done</button> 
             </span>
           </div> 
--- src/store/reducers/todolist.jsx
+++ src/store/reducers/todolist.jsx
@@ -3,21 +3,26 @@ const intialState =
     todos : [
        
     ],
+    filteredTodos:[]
     
 }
 
 const todoReducer= function(state=intialState,action){
+    state.filteredTodos=[...state.todos]
     if(action.type==='ADDTODO'){
-        return {...state,todos:[...state.todos,{title:action.payload,status:false}]}
+        var todos = [...state,todos,{title:action.payload,status:false}]
+        return {...state,todos,filteredTodos:[...todos]}
     }
     if(action.type==='DELETE'){
-        return {...state,todos:[...state.todos.filter((each)=> { return each.title!==action.payload.title})]}
-        
+        return {...state,todos:[...state.todos.filter((each,i)=> i!==action.payload)]}   
     }
-    if(action.type==='DONE'){
-        let temp ={...state.todos}
-        temp[action.payload].status=true
-        return {...state,todos:[...temp]}
+    // if(action.type==='DONE'){
+    //     let temp ={...state.todos}
+    //     temp[action.payload].status=true
+    //     return {...state,todos:[...temp]}
+    // }
+    if(action.type==='ALL'){
+        return {...state,filteredTodos:[...state.todos]}
     }
     return state
 }
