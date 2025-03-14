d621ca2d5d86dd4c915c7f7c29193e47c43c5768
punugupatisairam@gmail.com <punugupatisairam@gmail.com>
2025-03-14 20:49:46 +0530

    optional _change_thing

--- src/Myapicall.jsx
+++ src/Myapicall.jsx
@@ -3,20 +3,25 @@ import axios from "axios";
 
 function Myapicall() {
 
-    var [res,setRes] = useState(null)
+    var [res,setRes] = useState([])
+    let [data,SetData]=useState(null)
     useEffect(()=>{
         axios.get('https://restcountries.com/v3.1/all').then((data)=>{
-            console.log(data)
+            // console.log(data)
             setRes(data.data)
         })
     },[])
-
+    // console.log(res)
+    console.log(data?.name) //optional change good to use, it will show undefined instead of null   
    
   return (
     <div>
         {
             res.map((p)=>{
-                return <li>{p.capital}</li>
+                return <div>
+                            <span>{p.capital}</span> &nbsp;, &nbsp;
+                            <span>{p.name.common}</span>
+                        </div> 
             })
         }
      
